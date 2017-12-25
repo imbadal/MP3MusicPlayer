daa09574a6803972c073430289206f53c6827e76
Badal kumar <s.k.badal@outlook.com>
2017-12-25 22:25:22 +0530

    Player Screen 1.2 Equilizer 2.1

--- app/src/main/java/com/example/my_lenovo/mp3musicplayer/MainActivity.java
+++ app/src/main/java/com/example/my_lenovo/mp3musicplayer/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.my_lenovo.mp3musicplayer;
 
-import android.graphics.Color;
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.ImageView;
 
 import com.marcinmoskala.arcseekbar.ArcSeekBar;
-import com.sdsmdg.harjot.crollerTest.Croller;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    int i =0;
+    ImageView Play;
+    ImageView Favourite;
 
     /**
      *Variable declaration of ArcSeekBar
@@ -21,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
      *Variable declaration of ArcSeekBar
      * End
      */
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,8 +47,35 @@ public class MainActivity extends AppCompatActivity {
          * End
          */
 
+        Play= (ImageView) findViewById(R.id.play_pause);
+        final MediaPlayer mp = MediaPlayer.create(MainActivity.this,R.raw.swagseswagat);
+        Play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mp.isPlaying()){
+                    mp.pause();
+                    Play.setBackgroundResource(R.drawable.ic_play_circle_outline_white_48dp);
+                }else {
+                    mp.start();
+                    Play.setBackgroundResource(R.drawable.ic_pause_circle_outline_white_48dp);
+                }
 
+            }
+        });
 
+        Favourite = (ImageView) findViewById(R.id.favourite);
+        Favourite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (i==0){
+                    Favourite.setBackgroundResource(R.drawable.ic_favorite_white_48dp);
+                    i=1;
+                }else{
+                    Favourite.setBackgroundResource(R.drawable.ic_favorite_border_white_48dp);
+                    i=0;
+                }
+            }
+        });
     }
 
 
