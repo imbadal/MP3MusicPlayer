d049dc8f207680e398afaf7b387885b8d037038f
Badal kumar <s.k.badal@outlook.com>
2018-01-02 19:43:06 +0530

    Player Screen 2.2 Minimize Application

--- app/src/main/java/com/example/my_lenovo/mp3musicplayer/MainActivity.java
+++ app/src/main/java/com/example/my_lenovo/mp3musicplayer/MainActivity.java
@@ -1,5 +1,15 @@
 package com.example.my_lenovo.mp3musicplayer;
 
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 import android.annotation.SuppressLint;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
@@ -11,19 +21,20 @@ import com.marcinmoskala.arcseekbar.ArcSeekBar;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION = 2084;
 
-    int i =0;
+    int i = 0;
     ImageView Play;
     ImageView Favourite;
 
     /**
-     *Variable declaration of ArcSeekBar
+     * Variable declaration of ArcSeekBar
      * Start
      */
     ArcSeekBar gradientSeek;
 
     /**
-     *Variable declaration of ArcSeekBar
+     * Variable declaration of ArcSeekBar
      * End
      */
     @SuppressLint("WrongViewCast")
@@ -31,6 +42,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Check if the application has draw over other apps permission or not?
+        //This permission is by default available for API<23. But for API > 23
+        //you have to ask for the permission in runtime.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+
+
+            //If the draw over permission is not available open the settings screen
+            //to grant the permission.
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, CODE_DRAW_OVER_OTHER_APP_PERMISSION);
+        } else {
+            initializeView();
+        }
 
         /**
          *ArcSeekBar Code
@@ -47,15 +73,15 @@ public class MainActivity extends AppCompatActivity {
          * End
          */
 
-        Play= (ImageView) findViewById(R.id.play_pause);
-        final MediaPlayer mp = MediaPlayer.create(MainActivity.this,R.raw.swagseswagat);
+        Play = (ImageView) findViewById(R.id.play_pause);
+        final MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.swagseswagat);
         Play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mp.isPlaying()){
+                if (mp.isPlaying()) {
                     mp.pause();
                     Play.setBackgroundResource(R.drawable.ic_play_circle_outline_white_48dp);
-                }else {
+                } else {
                     mp.start();
                     Play.setBackgroundResource(R.drawable.ic_pause_circle_outline_white_48dp);
                 }
@@ -67,16 +93,42 @@ public class MainActivity extends AppCompatActivity {
         Favourite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (i==0){
+                if (i == 0) {
                     Favourite.setBackgroundResource(R.drawable.ic_favorite_white_48dp);
-                    i=1;
-                }else{
+                    i = 1;
+                } else {
                     Favourite.setBackgroundResource(R.drawable.ic_favorite_border_white_48dp);
-                    i=0;
+                    i = 0;
                 }
             }
         });
     }
 
+    private void initializeView() {
+        findViewById(R.id.minimize).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, FloatingViewService.class));
+                finish();
+            }
+        });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION) {
+            //Check if the permission is granted or not.
+            if (resultCode == RESULT_OK) {
+                initializeView();
+            } else { //Permission is not available
+                Toast.makeText(this,
+                        "Draw over other app permission not available. Closing the application",
+                        Toast.LENGTH_SHORT).show();
+
+                finish();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
